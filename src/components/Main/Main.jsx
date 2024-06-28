351839533afeff1d276be394f3a6c0bd243b8c32
bula1623 <bulla1623@gmail.com>
2024-06-28 17:59:11 +0530

    Add animations to components, responsive design.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -82,7 +82,7 @@ const Main = () => {
                         <div>
                             <img src={assets.gallery_icon} alt="" />
                             <img src={assets.mic_icon} alt="" />
-                            <img src={assets.send_icon} alt="" onClick={() => { onSent() }} />
+                            {input?<img src={assets.send_icon} alt="" onClick={() => { onSent() }} />:null}
                         </div>
                     </div>
                     <p className="bottom-info">
